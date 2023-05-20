de2cacc46fe770c4c2b72a7eab5407fa7c70baf6
LoZioo <60042676+LoZioo@users.noreply.github.com>
2023-05-20 20:40:37 +0200

    feat: first demo code.

--- images/outlier-detector/main.py
+++ images/outlier-detector/main.py
@@ -1,2 +1,65 @@
+from typing import Any
+import paho.mqtt.client as mqtt
+
+# App log.
+from os import environ
+from sys import stdout, stderr
+from typing import TextIO
+
+def log(message: str, file: TextIO = stdout, newline: bool = False) -> None:
+	HOSTNAME = environ["HOSTNAME"] if "HOSTNAME" in environ else "Log"
+
+	print("%s[%s] %s" % ("\n" if newline else "", HOSTNAME, message), file=file)
+
+# Protocol can be also "websocket".
+client = mqtt.Client(
+	client_id = "outlier-detector",
+	transport = "tcp",
+	protocol = mqtt.MQTTv311,
+	clean_session = True
+)
+
+# MQTT Callbacks.
+def on_connect(client: Any, userdata: Any, flags: Any, rc: Any) -> None:
+	log("Connected to %s!" % MQTT_BROKER)
+
+def on_disconnect(client: Any, userdata: Any, flags: Any) -> None:
+	log("Disconnected from %s." % MQTT_BROKER)
+
+# Graceful shutdown.
+from signal import signal, SIGINT
+
+def graceful_shutdown(signal: int, frame: Any) -> None:
+	log("SIGINT detected, exiting...", stdout, True)
+
+	client.loop_stop()
+	client.disconnect()
+
 if __name__ == "__main__":
-	print("I'm the outlier-detector!")
+	log("I'm the outlier-detector!")
+
+	# Attach callbacks.
+	client.on_connect = on_connect
+	client.on_disconnect = on_disconnect
+
+	signal(SIGINT, graceful_shutdown)
+
+	# Retrive MQTT_BROKER envroiment variable.
+	if "MQTT_BROKER" in environ:
+		MQTT_BROKER = environ["MQTT_BROKER"]
+	
+	else:
+		log("MQTT_BROKER envroiment variable not set, aborting.", stderr)
+		exit(1)
+
+	# Connect to the broker.
+	log("Connecting to %s..." % MQTT_BROKER)
+	try:
+		client.connect(MQTT_BROKER)
+
+	except Exception as e:
+		log("Connection to %s failed: %s." % (MQTT_BROKER, e), stderr)
+		exit(1)
+
+	# Begin MQTT event loop.
+	client.loop_forever()
