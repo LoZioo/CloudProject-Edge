d05ffcf285611518e8d8e06b405f65f7de58a697
LoZioo <60042676+LoZioo@users.noreply.github.com>
2023-05-20 18:06:15 +0200

    fix

--- clean.sh
+++ clean.sh
@@ -4,8 +4,9 @@
 source config.sh
 
 docker compose rm -f
+cd images
 
-for image in $(ls -d images/*/ | sed "s/\///g")
+for image in $(ls -d */ | sed "s/\///g")
 do
 	docker image rm $DOCKERHUB_USER/$image
 done
