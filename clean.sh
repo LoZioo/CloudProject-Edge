ccc63e143e6b4ea0ae950c1ca845b2ffd7c4dd9e
LoZioo <60042676+LoZioo@users.noreply.github.com>
2023-05-21 12:54:38 +0200

    Revert "feat: removed clean.sh"

---
+++ clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd images
+
+# Include common variables.
+source config.sh
+
+docker compose kill
+docker compose rm -f
+
+for image in $(ls -d */ | sed "s/\///g")
+do
+	docker image rm $DOCKERHUB_USER/$image
+done
