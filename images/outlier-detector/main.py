31ad2c15b48d7deb2491ba512779e484d86566a2
LoZioo <60042676+LoZioo@users.noreply.github.com>
2023-05-21 14:15:16 +0200

    feat: SAMPLES_LEN

--- images/outlier-detector/main.py
+++ images/outlier-detector/main.py
@@ -1,5 +1,10 @@
 from typing import Any
 import paho.mqtt.client as mqtt
+import json
+
+# Envroiment vaiables:
+# MQTT_BROKER: str =		IP address of the MQTT Broker.
+# SAMPLES_LEN: int =		Trigger transmission routine on that defined len(data).
 
 # Command topic.
 MQTT_REQUEST_TOPIC = "/PowerMonitor"
@@ -7,13 +12,21 @@ MQTT_REQUEST_TOPIC = "/PowerMonitor"
 # Telemetry topic.
 MQTT_DATA_TOPIC = "/PowerMonitor/data"
 
+# VA and W array.
+SAMPLES_T = list[tuple[float, float]]
+samples: SAMPLES_T = []
+
 # App log.
 from os import environ
 from sys import stdout, stderr
 from typing import TextIO
 
-def log(message: str, tag: str = "Info", file: TextIO = stdout, newline: bool = False) -> None:
+def log(message: Any, tag: str = "Info", file: TextIO = stdout, newline: bool = False) -> None:
 	print("%s[%s] %s" % ("\n" if newline else "", tag, message), file=file)
+
+# Functions
+def handle_samples(samples: SAMPLES_T) -> None:
+	log(samples, "Samples")
 
 # Protocol can be also "websocket".
 client = mqtt.Client(
@@ -37,7 +50,19 @@ def on_message(client: Any, userdata: Any, message: mqtt.MQTTMessage) -> None:
 	TOPIC = message.topic
 	MESSAGE = message.payload.decode("utf-8")
 
-	log(MESSAGE, tag=TOPIC)
+	if TOPIC == MQTT_DATA_TOPIC:
+		samples.append((
+			json.loads(MESSAGE)["p"]["VA"],
+			json.loads(MESSAGE)["p"]["W"]
+		))
+
+		if len(samples) == SAMPLES_LEN:
+			log("Reached pefixed len of %d samples." % SAMPLES_LEN, "Trigger")
+
+			samples_copy = samples.copy()
+			samples.clear()
+
+			handle_samples(samples_copy)
 
 # Graceful shutdown.
 from signal import signal, SIGINT
@@ -64,8 +89,15 @@ if __name__ == "__main__":
 		MQTT_BROKER = environ["MQTT_BROKER"]
 
 	else:
-		log("MQTT_BROKER envroiment variable not set, aborting.", "Error", stderr)
-		log("Exiting...")
+		log("MQTT_BROKER envroiment variable not set, exiting...", "Error", stderr)
+		exit(1)
+
+	# Retrive SAMPLES_LEN envroiment variable.
+	if "SAMPLES_LEN" in environ:
+		SAMPLES_LEN = int(environ["SAMPLES_LEN"])
+
+	else:
+		log("SAMPLES_LEN envroiment variable not set, exiting...", "Error", stderr)
 		exit(1)
 
 	# Connect to the broker.
